f5d0b884b37ae6ed6ef82606733c1c43bbf295c1
EllenDown <ellenldowning@gmail.com>
2017-06-17 19:37:14 -0600

    Commit without divide by zero error

--- js/index.js
+++ js/index.js
@@ -53,11 +53,13 @@ function evaluateEquation() {
       if ($screenText[i] === "+" || $screenText[i] === "-" || $screenText[i] === "*" || $screenText[i] === "/") {
         counter++;
       }
+      if ($screenText.includes("/0")) {
+        console.log("hi")
+        $screen.text("Error");
+      }
       if (counter > 1) {
         $screen.text("Error");
       } else if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
-        $screen.text("Error");
-      } else if ($screenText.includes("/" && lastChar === $zero)) {
         $screen.text("Error");
       } else {
         var result = eval($screenText)
@@ -68,36 +70,44 @@ function evaluateEquation() {
 }
 
 
-//   if ($screenText.includes("x")) {
-//     $screenText = $screenText.replace("x", "*")
-//   }
-//   if ($screenText.includes("÷")) {
-//     $screenText = $screenText.replace("÷", "/")
-//   }
-//   var lastChar = $screenText[$screenText.length - 1];
-//   var counter = 0;
-//   for (var i = 0; i < $screenText.length; i++) {
-//     if ($screenText[i] === "+" || $screenText[i] === "-" || $screenText[i] === "*" || $screenText[i] === "/") {
-//       counter++;
-//       console.log(counter)
-//     }
-//     if (counter > 1) {
-//       $screen.text("Error");
-//     } else if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
-//       $screen.text("Error");
-//     } else {
-//       var result = eval($screenText)
-//       $screen.text(result);
-//     }
-//   }
-// }
-// } else if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
-//
-//   $screen.text("Error");
-// } else if ($screenText.includes(('/') && lastChar === "0")) {
-//   $screen.text("Error")
-//
-// } else {
 
-// var result = eval($screenText)
-// $screen.text(result);
+  // function infinityCheck() {
+  //   var result = eval($screenText)
+  //   if (result === "Infinity") {
+  //     console.log($screenText)
+  //     $screen.text("Error")
+  //   }
+
+  //   if ($screenText.includes("x")) {
+  //     $screenText = $screenText.replace("x", "*")
+  //   }
+  //   if ($screenText.includes("÷")) {
+  //     $screenText = $screenText.replace("÷", "/")
+  //   }
+  //   var lastChar = $screenText[$screenText.length - 1];
+  //   var counter = 0;
+  //   for (var i = 0; i < $screenText.length; i++) {
+  //     if ($screenText[i] === "+" || $screenText[i] === "-" || $screenText[i] === "*" || $screenText[i] === "/") {
+  //       counter++;
+  //       console.log(counter)
+  //     }
+  //     if (counter > 1) {
+  //       $screen.text("Error");
+  //     } else if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
+  //       $screen.text("Error");
+  //     } else {
+  //       var result = eval($screenText)
+  //       $screen.text(result);
+  //     }
+  //   }
+  // }
+  // } else if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
+  //
+  //   $screen.text("Error");
+  // } else if ($screenText.includes(('/') && lastChar === "0")) {
+  //   $screen.text("Error")
+  //
+  // } else {
+
+  // var result = eval($screenText)
+  // $screen.text(result);
